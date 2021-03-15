6725c6eb491e80ad319b61f689cf973155b79a18
emanuelpras <78716239+emanuelpras@users.noreply.github.com>
2021-03-15 12:13:30 +0700

    Delete xapiens-project-gin-go directory

--- xapiens-project-gin-go/controller/gorm.go
+++
@@ -1,7 +0,0 @@
-package controller
-
-import "gorm.io/gorm"
-
-type StrDB struct {
-	DB *gorm.DB
-}
--- xapiens-project-gin-go/others/fungsiSederhana.go
+++
@@ -1,8 +0,0 @@
-package main
-
-func FungsiSederhana(valueA, valueB float64) float64 {
-	var result float64
-	result = (valueA - valueB) + 2*valueA
-
-	return result
-}
